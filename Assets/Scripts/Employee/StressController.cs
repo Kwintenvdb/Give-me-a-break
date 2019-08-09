0fa57513ae74f01e0d0cc7ff79234c53fb71a0ab
Kwinten Van den berghe <Kwinten.Vandenberghe@catalysts.cc>
2019-08-09 19:55:54 +0200

    Employee, stress controller, office components

---
+++ Assets/Scripts/Employee/StressController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StressController : MonoBehaviour
+{
+    [SerializeField] private float stressLevel = 50;
+    [SerializeField] private float stressGrowthPerSecond = 2;
+
+    // Update is called once per frame
+    void Update()
+    {
+        stressLevel += stressGrowthPerSecond * Time.deltaTime;
+    }
+
+    public float GetStressLevel()
+    {
+        return stressLevel;
+    }
+}
---
+++ Assets/Scripts/Office/Office.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Office : MonoBehaviour
+{
+    void Update()
+    {
+        var employees = FindObjectsOfType<Employee>();
+        float averagePercentageStress = employees.Average(employee => employee.GetPercentageStress());
+        Debug.Log(averagePercentageStress);
+    }
+}
